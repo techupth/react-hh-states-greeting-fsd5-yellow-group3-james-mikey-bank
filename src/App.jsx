806e159d89957c600094a52e8de16e944b34a1dc
lacitadelle <jkomala99@gmail.com>
2023-12-18 21:36:50 +0700

    added buttons for greeting selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+
+  function handleClick(newGreeting) {
+    setGreeting(newGreeting);
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={(e) => handleClick(`${e.target.innerText}`)}>
+          สวัสดี!
+        </button>
+        <button onClick={(e) => handleClick(`${e.target.innerText}`)}>
+          Hi!
+        </button>
+        <button onClick={(e) => handleClick(`${e.target.innerText}`)}>
+          你好!
+        </button>
       </div>
     </div>
   );
